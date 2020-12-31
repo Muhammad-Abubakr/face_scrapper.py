4eed0bebd974af515b1aec6311dcda7e2d77b8c5
frostywolf <74457014+Muhammad-Abubakr@users.noreply.github.com>
2020-12-31 22:23:41 +0500

    Error Handling

--- Face_Scrapper/face_scrapper.py
+++ Face_Scrapper/face_scrapper.py
@@ -2,45 +2,50 @@ import cv2
 import sys
 import os, os.path
 
-image_path = sys.argv[1]
-output_path = sys.argv[2]
-
-if os.path.exists(f'{image_path}/{output_path}'):
-    pass
-else:
-    os.makedirs(f'{image_path}/{output_path}')
-
 try:
-	for img in os.listdir(f'{image_path}/'):
-		if img.endswith('.jpg') or img.endswith('.png') or img.endswith('.webp') or img.endswith('.jpeg'):
-			image = cv2.imread(f'{image_path}/{img}')
-			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_path = sys.argv[1]
+    output_path = sys.argv[2]
 
-			faceCascade = cv2.CascadeClassifier(
-			    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-			faces = faceCascade.detectMultiScale(
-			    gray,
-			    scaleFactor=1.3,
-			    minNeighbors=3,
-			    minSize=(30, 30)
-			)
-
-			print(f"Found {len(faces)} Faces!")
-
-			i = 0
-			for (x, y, w, h) in faces:
-			    crop_img = image[y:y+h, x:x+w]
-			    i = i+1
-			    if os.path.exists(f'{image_path}/{output_path}/{img}'):
-			    	pass
-			    else:
-			    	os.makedirs(f'{image_path}/{output_path}/{img}')
-			    	
-			    status = cv2.imwrite(f'{image_path}/{output_path}/{img}/{str(i)}.png', crop_img)
-			    print('done!')
-			    continue
-		continue
-
+    if os.path.exists(f'{image_path}/{output_path}'):
+    	pass
+    else:
+    	os.makedirs(f'{image_path}/{output_path}')
 except:
-	print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
+    print('Enter the Command like following : \n python3 face_scrapper.py {Image_Path} {New_Folder_Name} \n . \n . \n . \n Attention! Enter the path without {}, {} are only for user understanding :)')
+
+
+
+for img in os.listdir(f'{image_path}/'):
+    if img.endswith('.jpg') or img.endswith('.png') or img.endswith('.webp') or img.endswith('.jpeg'):
+        image = cv2.imread(f'{image_path}/{img}')
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faceCascade = cv2.CascadeClassifier(
+            cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.3,
+            minNeighbors=3,
+            minSize=(30, 30)
+        )
+
+        print(f"Found {len(faces)} Faces!")
+
+        i = 0
+        for (x, y, w, h) in faces:
+            crop_img = image[y:y+h, x:x+w]
+
+            if os.path.exists(f'{image_path}/{output_path}/{img}'):
+                pass
+            else:
+                os.makedirs(f'{image_path}/{output_path}/{img}')
+
+            status = cv2.imwrite(f'{image_path}/{output_path}/{img}/{str(i)}.png', crop_img)
+
+            i = i+1
+            print('done!')
+            continue
+    else:
+    	print('No images to process in the given directory!')
+    continue
 
